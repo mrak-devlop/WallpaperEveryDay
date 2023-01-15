a68001394c07e21ec92347ea1077d4d64586176a
mrak-devlop <morok2devil@gmail.com>
2023-01-15 21:33:14 +0400

    update dependencies

---
+++ app/src/main/java/ru/kitfactory/wallpapereveryday/service/WallpaperEveryDayService.kt
@@ -0,0 +1,12 @@
+package ru.kitfactory.wallpapereveryday.service
+
+
+import android.service.wallpaper.WallpaperService
+
+class WallpaperEveryDayService : WallpaperService() {
+    override fun onCreateEngine(): Engine {
+        TODO("Not yet implemented")
+    }
+
+
+}
